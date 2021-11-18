e06f08ebe13a04e2579668e48fbc6f168350ee40
mamark90 <maqmark90@gmail.com>
2021-11-17 22:54:10 -0500

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+
+  // componentDidMount() {
+  //   fetch("http://localhost:3001/api/v1/categories/1/items/1", {
+  //     method: "GET",
+  //   }) 
+  //     .then(response => response.json())
+  //     .then(data => console.log(data))
+
+  //   }
+
+  render() {
+    return (
+      <div className="App">
+        App
+      </div>
+  )
+ }
+
 }
 
 export default App;
